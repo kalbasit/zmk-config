d98f953942c73f542a20a621cb3025c10e9eaf54
Wael Nasreddine <wael.nasreddine@gmail.com>
2025-11-14 10:14:48 -0800

    update flakes

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
 
     zmk-nix = {
       url = "github:lilyinstarlight/zmk-nix";
